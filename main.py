bcf097d155ee8f4fce6e0cf869d9acc19216cdda
TlsMileto <72638273@continental.edu.pe>
2024-09-02 18:52:15 -0500

    Proyecto de suma en POO

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+from OperaciomesAritmeticas import OperaciomesAritmeticas
 
+
+if __name__=='__main__':
+    operación = OperaciomesAritmeticas()
+    num1, num2 = operación.ingresoNumeros()
+    print(f"{num1} + {num2}= {operación.suma(num1, num2)}")
 
